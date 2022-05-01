a59f8401a1f21a903e61ce76b57265bb4bde165c
halikali <tunchalikali@gmail.com>
2022-05-01 19:29:22 +0300

    change config settings for next.js image component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["images.unsplash.com"],
+    formats: ["image/webp", "image/avif"],
+    optimizeImages: true,
+    deviceSizes: [375, 768, 1024, 1200, 1600],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
