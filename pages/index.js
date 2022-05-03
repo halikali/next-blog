53cd6b26c5018bb11e02b3ac8795336ef6f249ad
halikali <tunchalikali@gmail.com>
2022-05-03 15:41:01 +0300

    import the popular post component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import LandingPost from "../src/components/LandingPost/LandingPost";
+import PopularPosts from "../src/components/PopularPosts/PopularPosts";
 
 export default function Home() {
   return (
     <>
       <LandingPost />
+      <PopularPosts />
     </>
   );
 }
