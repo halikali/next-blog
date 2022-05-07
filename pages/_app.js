8b37234b356849df8b49769b37ac77145a164db4
halikali <tunchalikali@gmail.com>
2022-05-08 02:56:15 +0300

    import styleshhet for global styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import MainLayout from "../src/components/Layout/MainLayout";
 import "../styles/reset.scss";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
