83838d57cc8cab38c45e997598755b54d8364d2d
halikali <tunchalikali@gmail.com>
2022-05-12 22:02:26 +0300

    complete author component

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,23 +1,49 @@
-import Image from "next/image";
-
+import { useEffect, useState } from "react";
 import style from "./Author.module.scss";
 
-const Author = () => {
+const Author = ({ info }) => {
+  const [createdAt, setCreatedAt] = useState("");
+  const [readTime, setReadTime] = useState(0);
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const formattedDate = (date) => {
+    const assignedDate = new Date(date);
+    const month = assignedDate.getMonth();
+    const day = assignedDate.getDate();
+    const year = assignedDate.getFullYear();
+
+    setCreatedAt(`${monthNames[month]} ${day},${year}`);
+  };
+
+  useEffect(() => {
+    formattedDate(info.postCreated);
+    setReadTime(Number.parseInt(Math.random() * 10 + 2));
+  }, []);
+
   return (
     <div className={style.author}>
       <div className={style.imageWrapper}>
-        <Image
-          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          width={56}
-          height={56}
-          layout="responsive"
-          className={style.image}
-          alt="Author"
-        />
+        <img src={info.avatarUrl} className={style.image} />
       </div>
       <div className={style.textWrapper}>
-        <h5 className={style.title}>mıka matıkaınen</h5>
-        <p className={style.createdAt}>Apr 15,2020 . 4 min read</p>
+        <h5 className={style.title}>{info.name}</h5>
+        <p className={style.createdAt}>
+          {`${createdAt}  `}
+          <span>{`${readTime} min. read `} </span>
+        </p>
       </div>
     </div>
   );
